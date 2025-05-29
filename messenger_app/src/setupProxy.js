e945e2dfa6f7b4f7df740324dd3e7acf5b550884
charan1147 <charanc1147@gmail.com>
2025-05-29 09:07:36 +0530

    changes

--- messenger_app/src/setupProxy.js
+++ messenger_app/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'https://whatsapp-backend-10.onrender.com',
+      target: 'https://whatsapp-backend-11.onrender.com',
       changeOrigin: true,
       secure: false
     })
